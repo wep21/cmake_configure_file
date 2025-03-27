bd8bd0ff981dd62665080419c7c58f9eac1a56c7
wep21 <daisuke.nishimatsu1021@gmail.com>
2025-03-27 21:05:56 +0900

    ci: add bcr auto deploy setting
    Signed-off-by: wep21 <daisuke.nishimatsu1021@gmail.com>

---
+++ .github/workflows/release_prep.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -o errexit -o nounset -o pipefail
+
+TAG="$1"
+# The prefix is chosen to match what GitHub generates for source archives
+# This guarantees that users can easily switch from a released artifact to a source archive
+# with minimal differences in their code (e.g. strip_prefix remains the same)
+PREFIX="cmake_configure_file-${TAG:1}"
+ARCHIVE="cmake_configure_file-$TAG.tar.gz"
+
+# NB: configuration for 'git archive' is in /.gitattributes
+git archive --format=tar --prefix=${PREFIX}/ ${TAG} | gzip > $ARCHIVE
+SHA=$(shasum -a 256 $ARCHIVE | awk '{print $1}')
+
+cat << EOF
+## Using Bzlmod with Bazel 6 or greater
+
+1. (Bazel 6 only) Enable with \`common --enable_bzlmod\` in \`.bazelrc\`.
+2. Add to your \`MODULE.bazel\` file:
+
+\`\`\`starlark
+bazel_dep(name = "cmake_configure_file", version = "${TAG:1}")
+\`\`\`
+
+## Using WORKSPACE
+
+Paste this snippet into your \`WORKSPACE.bazel\` file:
+
+\`\`\`starlark
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+http_archive(
+    name = "cmake_configure_file",
+    sha256 = "${SHA}",
+    strip_prefix = "${PREFIX}",
+    url = "https://github.com/wep21/cmake_configure_file/releases/download/${TAG}/${ARCHIVE}",
+)
+\`\`\`
+EOF
